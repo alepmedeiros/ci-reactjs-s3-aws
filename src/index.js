c0af7e9d90d261d971d1dae594edc29b5c1fcb23
alepmedeiros <alemedeiros@bsd.com.br>
2020-06-30 14:30:32 -0300

    alterecao das rotas

--- src/index.js
+++ src/index.js
@@ -41,8 +41,8 @@ ReactDOM.render(
     <Switch>
       <Route path="/admin" render={props => <AdminLayout {...props} />} />
       <Route path="/auth" render={props => <AuthLayout {...props} />} />
-      <Route path="/admin/dashboard" render={props => <IndexView {...props} />} />
-      <Redirect from="*" to="/admin/dashboard" />
+      <Route path="/" render={props => <IndexView {...props} />} />
+      <Redirect from="*" to="/" />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
