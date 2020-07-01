36035f06ba33ce1ba42d437fd31defe504b71ccf
alepmedeiros <alemedeiros@bsd.com.br>
2020-07-01 16:12:19 -0300

    atualizado Header novamente

--- src/components/Headers/HeaderGeneric.jsx
+++ src/components/Headers/HeaderGeneric.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { Container } from "reactstrap";
 
-export default class HeaderGeneric2 extends Component {
+export default class HeaderGeneric extends Component {
     render() {
         return (
             <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
             <Container fluid>
-                <div className="header-body">
-                {/* Card stats */}
-                
+                <div className="header-body">                
                 </div>
             </Container>
         </div>
