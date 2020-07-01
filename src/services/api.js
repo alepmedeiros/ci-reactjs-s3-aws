64b4cd69582f3de194a6dcc07f4b91474482466a
alepmedeiros <alemedeiros@bsd.com.br>
2020-07-01 14:24:39 -0300

    limpeza

--- src/services/api.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-//const api = axios.create({ baseURL : 'http://54.221.5.196:9000' });
-const api = axios.create({ baseURL : 'http://54.221.5.196:9200' });
-export default api;
--- src/services/apiAuth.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-//const api = axios.create({ baseURL : 'http://54.221.5.196:9000' });
-const apiAuth = axios.create({ baseURL : 'http://localhost:9001' });
-export default apiAuth;
